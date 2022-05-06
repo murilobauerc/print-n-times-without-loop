ee980d2e257568897861639a2a147ecbdda4e177
Murilo Bauer <murilo.bauer@sumup.com>
2022-05-06 16:29:13 -0300

    Update recursion.exs
    reduces the list to not pollute the code

--- recursion.exs
+++ recursion.exs
@@ -5,7 +5,7 @@ defmodule Recursion do
       1. Elixir installed
       2. Open an iex (interactive elixir shell) session, and call the function by typing: Recursion.print_n_times
   """
-  @list Enum.to_list(1..50)
+  @list Enum.to_list(1..10)
 
   @doc """
   Returns print of numbers n times
@@ -23,46 +23,6 @@ defmodule Recursion do
       8
       9
       10
-      11
-      12
-      13
-      14
-      15
-      16
-      17
-      18
-      19
-      20
-      21
-      22
-      23
-      24
-      25
-      26
-      27
-      28
-      29
-      30
-      31
-      32
-      33
-      34
-      35
-      36
-      37
-      38
-      39
-      40
-      41
-      42
-      43
-      44
-      45
-      46
-      47
-      48
-      49
-      50
       "end!"
   """
   def loop([head | tail]) do
